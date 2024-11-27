0b7d6c305eb28cf8b1c1e068242740a9da562485
ajay-addu <panduajay098@gmail.com>
2024-11-27 11:36:53 +0530

    Update p1.java

--- p1.java
+++ p1.java
@@ -1,5 +1,5 @@
 class p1{
     public static void main(String[] args) {
-        System.out.println("hii in lab i chaging code according to question");
+        System.out.println("hii in lab set 2 i got");
     }
 }
